fe55fbbb9dc84c856688a66864060e5f674f587f
Khusein Bespalyi <khusein.bespalyi@otolane.com>
2022-01-24 16:00:53 -0500

    exposed stateful collections to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,14 @@ import * as ReactDOM from 'react-dom';
 
 import App from 'App';
 
+export { useBooleanState as useBoolean } from './useBooleanState';
+export type { UseBoolean } from './useBooleanState';
+
+export { useMapState as useMap } from './useMapState';
+export type { UseMap } from './useMapState';
+export type { MapOrEntries } from './array/useMap';
+
+export { useStateful } from './useStateful';
+export type { UseStateful } from './useStateful';
+
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
